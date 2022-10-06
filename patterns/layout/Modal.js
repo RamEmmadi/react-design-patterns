859cfcf08cd1efe991cc4e5fe3201019f5de5e27
RamEmmadi <91928625+RamEmmadi@users.noreply.github.com>
2022-10-05 23:06:11 -0500

    1 file added

---
+++ patterns/layout/Modal.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ModalBackground = styled.div`
+  position: fixed;
+  width:100%;
+  height:100%;
+  left:0;
+  right:0;
+  overflow:auto;
+  background-color:rgba(0,0,0,0.5)
+`;
+
+const ModalHeading = styled.div`
+  background-color:white;
+  margin: 10% auto;
+  padding: 20px;
+  width: 50%
+  borderBotttom: 2px solid;
+`;
+
+const ModalBody = styled.div`
+  background-color:white;
+  margin: 10% auto;
+  padding: 20px;
+  width: 50%
+`;
+
+export const Modal = () => {
+  const [showModal, setShowModal] = React.useState(false);
+
+  return (
+    <>
+      {showModal && (
+        <ModalBackground onClick={() => setShowModal(false)}>
+          <ModalHeading>
+            Modal Heading
+            <button onClick={() => setShowModal(false)}>HIDE</button>
+          </ModalHeading>
+          <ModalBody>Modal Body</ModalBody>
+        </ModalBackground>
+      )}
+    </>
+  );
+};
